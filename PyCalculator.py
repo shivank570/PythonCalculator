ca6d87316b5e5961d2020444f3cbf1e4ebb559f1
Shivank Khanna <31126665+shivank570@users.noreply.github.com>
2021-07-21 15:33:06 +0530

    Add files via upload

---
+++ PyCalculator.py
@@ -0,0 +1,19 @@
+number1 = int(input("Choose number 1: "))
+oper = input("Choose a valid operation (+, -, *, /): ")
+number2 = int(input("Choose number 2: "))
+
+if oper == "+":
+       print("The answer is: " + str(number1+number2))
+elif oper == "-":
+       print ("The answer is: " + str(number1-number2))
+elif oper == "*":
+       print ("The answer is: " + str(number1*number2))
+elif oper == "/":
+        print ("The answer is: " + str(number1/number2))
+
+
+
+
+
+
+
